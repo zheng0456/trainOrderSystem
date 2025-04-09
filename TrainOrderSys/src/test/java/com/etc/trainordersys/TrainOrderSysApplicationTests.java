7d694627992ff4cdb49d08dba9a336e75668b00b
zheng0456 <165043942+zheng0456@users.noreply.github.com>
2025-04-09 10:03:05 +0800

    Add files via upload

---
+++ TrainOrderSys/src/test/java/com/etc/trainordersys/TrainOrderSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etc.trainordersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainOrderSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
