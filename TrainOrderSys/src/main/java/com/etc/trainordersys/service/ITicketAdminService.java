5e2dbc7349016e637f300aca100d2e030fed5886
xjy-12345 <2354602309@qq.com>
2025-05-07 15:57:19 +0800

    111

--- TrainOrderSys/src/main/java/com/etc/trainordersys/service/ITicketAdminService.java
+++ TrainOrderSys/src/main/java/com/etc/trainordersys/service/ITicketAdminService.java
@@ -5,5 +5,6 @@ import com.etc.trainordersys.entity.TicketEntity;
 import java.util.List;
 
 public interface ITicketAdminService {
+    //显示车票列表
     List<TicketEntity> findAllTicketPageList(String name);
 }
