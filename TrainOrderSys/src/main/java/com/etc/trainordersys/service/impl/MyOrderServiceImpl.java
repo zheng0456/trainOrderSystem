2cc5d0236d07d35db98cbf9d5a6709b682600fbc
MrZhao-520 <2606742122@qq.com>
2025-05-11 14:21:07 +0800

    支付部分

--- TrainOrderSys/src/main/java/com/etc/trainordersys/service/impl/MyOrderServiceImpl.java
+++ TrainOrderSys/src/main/java/com/etc/trainordersys/service/impl/MyOrderServiceImpl.java
@@ -30,22 +30,27 @@ public class MyOrderServiceImpl implements IMyOrderService {
     }
     //1.根据order_no,departure_date将t_order表中的order_status的值改为0，0代表已取消
     @Override
-    public boolean updateOrder(Integer orderNo, String departureDate) {
-        return myOrderMapper.updateOrder(orderNo,departureDate);
+    public boolean updateOrder(String orderNo) {
+        return myOrderMapper.updateOrder(orderNo);
     }
     //2.1根据order_no,departure_date查询t_ticket表
     @Override
-    public List<TicketEntity> showMyTrainDetailByDate(Integer orderNo, String departureDate) {
-        return myOrderMapper.showMyTrainDetailByDate(orderNo,departureDate);
+    public List<TicketEntity> showMyTrainDetail(String orderNo) {
+        return myOrderMapper.showMyTrainDetail(orderNo);
     }
     //2.2 循环修改t_ticket表中的ticket_status为6，6--已取消
     @Override
-    public boolean updateTicketStatus(int ticketId,String departureDate) {
-        return myOrderMapper.updateTicketStatus(ticketId,departureDate);
+    public boolean updateTicketStatus(int ticketId,int ticketStatus) {
+        return myOrderMapper.updateTicketStatus(ticketId,ticketStatus);
     }
     //3.根据train_number,seat_type_id将t_schedule_seat_info表中的remain_nums+1
     @Override
     public boolean updateTrainSeatNum(String trainNumber, int seatTypeId) {
         return myOrderMapper.updateTrainNum(trainNumber,seatTypeId);
+    }
+    //支付成功后，修改订单状态和支付状态
+    @Override
+    public boolean updateOrderStatus(String outTradeNo) {
+        return myOrderMapper.updateOrderStatus(outTradeNo);
     }
 }
