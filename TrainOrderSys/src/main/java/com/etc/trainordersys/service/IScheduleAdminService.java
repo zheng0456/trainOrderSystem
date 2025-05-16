5afc1b1c0d47d7ef9b458adfbdfe80ff6c1ffd7f
xjy-12345 <2354602309@qq.com>
2025-05-16 16:03:35 +0800

    22222222222

---
+++ TrainOrderSys/src/main/java/com/etc/trainordersys/service/IScheduleAdminService.java
@@ -0,0 +1,22 @@
+package com.etc.trainordersys.service;
+
+import com.etc.trainordersys.entity.StationEntity;
+import com.etc.trainordersys.entity.TrainCarriageEntity;
+import com.etc.trainordersys.entity.TrainScheduleEntity;
+
+import java.util.List;
+
+public interface IScheduleAdminService {
+    //显示车站列表
+    List<TrainScheduleEntity> findAllschedulePageList();
+    //查询所有车厢
+    List<TrainCarriageEntity> findAllCarriage();
+
+    List<StationEntity> findAllStationPageList();
+    //保存添加车次
+    String addSchedule(TrainScheduleEntity trainSchedule);
+    //显示编辑车次页面
+    TrainScheduleEntity findAllscheduleById(int id);
+    //保存编辑车次信息
+    String edit(TrainScheduleEntity trainSchedule);
+}
